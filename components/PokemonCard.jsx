47b8b9ddcc0956317f4f1d4423caadaee6ad94ad
PMorganGit <morganpouilly@gmail.com>
2024-03-26 19:50:19 +0100

    Card mew ???

--- components/PokemonCard.jsx
+++ components/PokemonCard.jsx
@@ -1,12 +1,32 @@
 function PokemonCard() {
-    return (
-      <div>
-        <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-    <figcaption>Bulbasaur</figcaption>
-    </figure>
-      </div>
-    );
-  }
-  
-  export default PokemonCard;
+  const pokemon = pokemonList[1];
+  console.log(pokemon);
+  const imgabs = typeof pokemon.imgSrc === "undefined";
+
+  return (
+    <div>
+      <figure>
+        {imgabs ? (
+          <p>???</p>
+        ) : (
+          <img src={pokemon.imgSrc} alt={pokemon.name}></img>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    </div>
+  );
+}
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    alt: "bulbasaur",
+  },
+  {
+    name: "mew",
+  },
+];
+
+export default PokemonCard;
